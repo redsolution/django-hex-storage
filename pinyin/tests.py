ff2dee4a3009605ba0a481c1512da5ffd9bffa55
Alexander Ivanov <alexander.ivanov@redsolution.ru>
2010-03-29 17:24:47 +0600

    Put pinyin here

---
+++ pinyin/tests.py
@@ -0,0 +1,49 @@
+# -*- coding: UTF-8 -*-
+
+import unittest
+from urlify import urlify
+
+class PinYinTestCase(unittest.TestCase):
+    def testChinese(self):
+        self.assertEqual(urlify(u'公共的模'),
+                         u'gong-gong-de-mo')
+
+    def testEnglish(self): 
+        self.assertEqual(urlify(u'WOW! We Say ENGLISH!!!'),
+                         u'wow-we-say-english')
+
+    def testWestern(self): 
+        self.assertEqual(urlify(u'ÀÞβ Λğ-Ґє'),
+                         u'athb-lg-gye')
+
+    def testMixedText(self):
+        self.assertEqual(urlify(u'  公共的  模asdf!　'),
+                         u'gong-gong-de-mo-asdf')
+
+    def testHyphen(self):
+        self.assertEqual(urlify(u'公共-的模'),
+                         u'gong-gong-de-mo')
+
+    def testEmpty(self):
+        self.assertEqual(urlify(u''),
+                         u'default')
+
+    def testNotInMaps(self):
+        self.assertEqual(urlify(u'コにちわ'),
+                         u'default')
+
+    def testStopWords(self):
+        self.assertEqual(urlify(u'hello, this is an blahblah'),
+                         u'hello-blahblah')
+
+    def testReservedWords(self):
+        self.assertEqual(urlify(u'  blog  '),
+                         u'default')
+
+    def testMaxLengthLimit(self):
+        self.assertEqual(urlify(u'urlstring url instance class request embodies. Example, data headers, calling:'),
+                         u'urlstring-url-instance-class-request-embodies-exam')
+
+
+if __name__ == '__main__':
+    unittest.main()
