bd88bec514c8d17d55d4dd625579994c60b6438a
elky <mansellfan@gmail.com>
2010-09-07 13:11:54 +0600

    Renamed cms name

--- hex_storage/grandma_setup/make.py
+++
@@ -1,8 +0,0 @@
-from grandma.make import BaseMake
-from grandma.models import GrandmaSettings
-
-class Make(BaseMake):
-    def make(self):
-        super(Make, self).make()
-        grandma_settings = GrandmaSettings.objects.get_settings()
-        grandma_settings.render_to('settings.py', 'hex_storage/grandma/settings.py')
